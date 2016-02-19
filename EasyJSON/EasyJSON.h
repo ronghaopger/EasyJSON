3ed959e30305dd103d845d3d7d35f3cb44f57d93
荣浩 <ronghao_1989@hotmail.com>
2016-02-19 13:42:56 +0800

    v1.0.0
    实现了最基本的功能

---
+++ EasyJSON/EasyJSON.h
@@ -0,0 +1,19 @@
+//
+//  EasyJSON.h
+//  EasyJSON
+//
+//  Created by 荣浩 on 16/1/13.
+//  Copyright © 2016年 荣浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyJSON.
+FOUNDATION_EXPORT double EasyJSONVersionNumber;
+
+//! Project version string for EasyJSON.
+FOUNDATION_EXPORT const unsigned char EasyJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyJSON/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+use_frameworks!
+inhibit_all_warnings!
+pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
