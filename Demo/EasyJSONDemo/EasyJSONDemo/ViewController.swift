3af74d14977c4d6f4b5ebdd3a7c3c260314ac9ee
荣浩 <ronghao_1989@hotmail.com>
2016-02-19 16:30:22 +0800

    add Demo

---
+++ Demo/EasyJSONDemo/EasyJSONDemo/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  EasyJSONDemo
+//
+//  Created by 荣浩 on 16/2/19.
+//  Copyright © 2016年 荣浩. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let test = testModel()
+        test.createModel(["test1":"1", "test2":"2", "test33":"3", "test4":["11","12","13"], "test5":["name":"RH"]])
+        NSLog("%@", test.test4![0].age!)
+        NSLog("%@", test.test5!.name!)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
+public class testModel: NSObject {
+    public var test1: String?
+    public var test2: String?
+    public var test3: String?
+    public var test4: [arrayModel]?
+    public var test5: dictionaryModel?
+    
+    public override func specialMapping() -> [String: String]? {
+        return ["test3":"test33"]
+    }
+    
+    public override func arrayElementToModel() -> [String: String]? {
+        return ["test4":"arrayModel"]
+    }
+    
+    override   public func dictionaryToModel() -> [String: String]? {
+        return ["test5":"dictionaryModel"]
+    }
+}
+
+public class dictionaryModel:NSObject {
+    public var name: String?
+}
+
+public class arrayModel:NSObject {
+    public var age: String?
+}
